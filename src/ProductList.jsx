2ebfdcdd13be1e172cdf632c0b5f5cb7b75a8e5f
Iain Dunn <iain.dunn96@hotmail.co.uk>
2024-11-24 18:02:00 +0000

    disabled adding to cart after adding first time

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -316,7 +316,13 @@ function ProductList() {
                                         <div className="product-title">{plant.name}</div>
                                         <div>{plant.description}</div>
                                         <div className="product-price">${plant.cost}</div>
-                                        <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                        {cart.find(item => item.name === plant.name) ? (
+                                            <button className="product-button added-to-cart">Added to Cart</button>
+                                        ) : (
+                                            <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>  
+                                        )
+                                        }
+                                        
                                     </div>
                                 ))}
                             </div>
